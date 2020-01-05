b9bb892c9103df71f12c4091d3e936b5bbfd1c39
Anton <antonmakesgames@gmail.com>
2020-01-05 18:40:45 +0100

    Working on the json config file.

--- Dev/AutoClicker/ClickerServer/ClickerServer.h
+++ Dev/AutoClicker/ClickerServer/ClickerServer.h
@@ -16,7 +16,7 @@ class ClickerServer
 {
 public:
 	ClickerServer() {}
-	ClickerServer(utility::string_t url);
+	ClickerServer(utility::string_t url, const utility::string_t& config_file_path);
 
 	pplx::task<void> open() { 
 		return listener.open(); 
--- Dev/AutoClicker/ClickerServer/main.cpp
+++ Dev/AutoClicker/ClickerServer/main.cpp
@@ -2,20 +2,26 @@
 #include "ClickerServer.h"
 #include "AutoClicker.h"
 
+#include <map>
+#include <fstream>
+
+using namespace std;
+
 using namespace web;
 using namespace http;
 using namespace utility;
 using namespace http::experimental::listener;
 
+
 std::unique_ptr<ClickerServer> g_http;
 
-void on_initialize(const string_t& address)
+void on_initialize(const string_t& address, const string_t& config_filePath)
 {
 	uri_builder uri(address);
 	uri.append_path(U("AutoClicker/"));
 
 	string_t addr = uri.to_uri().to_string();
-	g_http = std::unique_ptr<ClickerServer>(new ClickerServer(addr));
+	g_http = std::unique_ptr<ClickerServer>(new ClickerServer(addr, config_filePath));
 	g_http->open().wait();
 
 	ucout << utility::string_t(U("Listening for requests at: ")) << addr << std::endl;
@@ -29,16 +35,38 @@ void on_shutdown()
 	return;
 }
 
-int main(int argc, wchar_t *argv[])
+int main(int argc, char *argv[])
 {
-	utility::string_t port = U("1234");
+	map<string_t, string_t> arguments;
+	string_t configPath = U("");
+	
+	if (argc > 0)
+	{
+		for (int index = 1; index < (argc - 1); index += 2)
+		{
+			string_t param = utility::conversions::to_string_t(argv[index]);
+			string_t value = utility::conversions::to_string_t(argv[index + 1]);
+			arguments[param] = value;
+		}
 
+		if (argc % 2 == 0)
+		{
+			configPath = utility::conversions::to_string_t(argv[argc - 1]);
+		}
+	}
+
+	if (configPath.size() > 0)
+	{
+
+	}
+	
+	utility::string_t port = U("1234");
 
 	utility::string_t address = U("http://localhost:");
 	address.append(port);
 
-	on_initialize(address);
-	std::cout << "Press ENTER to exit." << std::endl;
+	on_initialize(address, configPath);
+	std::wcout << "Press ENTER to exit." << std::endl;
 
 	std::string line;
 	std::getline(std::cin, line);
