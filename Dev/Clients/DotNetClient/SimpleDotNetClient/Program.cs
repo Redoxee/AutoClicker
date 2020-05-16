59c4674a1841d6740c375c1b85fdbef9702ab54b
Anton Roy <antonmakesgames@gmail.com>
2020-05-16 22:54:02 +0200

    Creating an example simple middleman bot.

---
+++ Dev/Clients/DotNetClient/SimpleDotNetClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace SimpleDotNetClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int logLaps = 10;
+
+            int clickPerSecond = 10;
+
+            int laps = 1000 / clickPerSecond;
+
+            while (true)
+            {
+                WebRequest request = WebRequest.Create("http://localhost:1414/AutoClicker?Click=1");
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                counter++;
+                if (counter >= logLaps)
+                {
+                    Console.WriteLine($"Request response.");
+                    counter = 0;
+                }
+
+                // limit the speed.
+                if (laps > 0)
+                {
+                    Thread.Sleep(laps);
+                }
+            }
+        }
+    }
+}
