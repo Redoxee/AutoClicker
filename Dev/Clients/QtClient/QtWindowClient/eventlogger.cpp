4943f80b2b9ee814c18e98dfe3b28ed622ea7ccf
Anton Roy <antonmakesgames@gmail.com>
2020-06-27 16:32:35 +0200

    Optimized the event logger.

--- Dev/Clients/QtClient/QtWindowClient/eventlogger.cpp
+++ Dev/Clients/QtClient/QtWindowClient/eventlogger.cpp
@@ -1,9 +1,8 @@
 #include "eventlogger.h"
 #include <QRandomGenerator>
+#include <QScrollBar>
 
-#include <QDebug>
-
-EventLogger::EventLogger(QWidget* parent) : QTextBrowser(parent)
+EventLogger::EventLogger(QWidget* parent) : QFrame(parent)
 {
     QString val;
     QFile file;
@@ -11,6 +10,7 @@ EventLogger::EventLogger(QWidget* parent) : QTextBrowser(parent)
     file.open(QIODevice::ReadOnly | QIODevice::Text);
     val = file.readAll();
     file.close();
+
     QJsonDocument d = QJsonDocument::fromJson(val.toUtf8());
     QJsonObject root = d.object();
     QJsonArray formats = root["Formats"].toArray();
@@ -26,11 +26,66 @@ EventLogger::EventLogger(QWidget* parent) : QTextBrowser(parent)
         QString value = fileNames[index].toString();
         this->fileList.append(value);
     }
+
+    this->setFrameStyle(QFrame::Box | QFrame::Sunken);
+    QPalette pal = palette();
+    pal.setColor(QPalette::Background, Qt::white);
+    this->setPalette(pal);
+    this->setAutoFillBackground(true);
+
+    this->randomGenerator = new QRandomGenerator();
+    this->stringList = new QStringList();
+
+    QVBoxLayout* mLayout = new QVBoxLayout(this);
+    mLayout->setMargin(0);
+    this->scrollArea = new QScrollArea(this);
+    mLayout->addWidget(this->scrollArea);
+
+    QWidget* scrollableWidget = new QWidget();
+    QVBoxLayout* vLayout = new QVBoxLayout(scrollableWidget);
+    scrollableWidget->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+    vLayout->setMargin(0);
+//    this->scrollArea->setWidgetResizable(true);
+
+    vLayout->setSpacing(0);
+    vLayout->setMargin(0);
+
+    this->labels = new QLabel*[EventLogger::maxSize];
+    for(int index = 0; index < EventLogger::maxSize; ++index)
+    {
+        this->labels[index] = new QLabel(this);
+        this->labels[index]->setMinimumHeight(10);
+        this->labels[index]->setMinimumWidth(250);
+        this->labels[index]->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
+        vLayout->addWidget(this->labels[index]);
+    }
+
+    this->scrollArea->setWidget(scrollableWidget);
 }
 
 void EventLogger::AppendRandomLog()
 {
-    int verbIndex = this->randomGenerator.bounded(this->verbList.count());
-    int fileIndex = this->randomGenerator.bounded(this->fileList.count());
-    this->append(this->verbList[verbIndex].arg(this->fileList[fileIndex]));
+    int verbIndex = this->randomGenerator->bounded(this->verbList.count());
+    int fileIndex = this->randomGenerator->bounded(this->fileList.count());
+    this->AppendString(this->verbList[verbIndex].arg(this->fileList[fileIndex]));
+}
+
+void EventLogger::AppendString(QString log)
+{
+    this->stringList->append(log);
+    if(this->stringList->count() >= this->maxSize)
+    {
+        this->stringList->removeAt(0);
+    }
+
+    int numberOfLogs = this->stringList->count();
+    for(int index = 0; index < this->maxSize; ++index)
+    {
+        if(index < numberOfLogs)
+        {
+            this->labels[this->maxSize - 1 - index]->setText(this->stringList->at(numberOfLogs - 1 - index));
+        }
+    }
+
+    this->scrollArea->verticalScrollBar()->setValue(this->scrollArea->verticalScrollBar()->maximum());
 }
--- Dev/Clients/QtClient/QtWindowClient/eventlogger.h
+++ Dev/Clients/QtClient/QtWindowClient/eventlogger.h
@@ -3,30 +3,39 @@
 
 #include <QVector>
 #include <QWidget>
-#include <QTextBrowser>
+#include <QFrame>
+#include <QLabel>
 #include <QString>
 #include <QFile>
 #include <QTextStream>
-#include <QStringList>
-#include <QUrl>
 #include <QJsonObject>
 #include <QJsonDocument>
 #include <QJsonArray>
 #include <QJsonValue>
 #include <QRandomGenerator>
+#include <QStringList>
+#include <QVBoxLayout>
+#include <QPalette>
+#include <QScrollArea>
 
-class EventLogger : public QTextBrowser
+class EventLogger : public QFrame
 {
     Q_OBJECT
 public:
     explicit EventLogger(QWidget* parent);
 
     void AppendRandomLog();
+    void AppendString(QString log);
 
 private:
     QVector<QString> verbList;
     QVector<QString> fileList;
-    QRandomGenerator randomGenerator;
+    QRandomGenerator* randomGenerator;
+
+    QStringList* stringList = nullptr;
+    QScrollArea* scrollArea = nullptr;
+    QLabel** labels = nullptr;
+    const int maxSize = 30;
 };
 
 #endif // EVENTLOGGER_H
